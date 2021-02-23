2f5e7cfb8d4789233144d2b98f857ea05cbc92f1
indianajmh <76800857+indianajmh@users.noreply.github.com>
2021-02-23 10:23:09 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ function show(shown, hidden) {
 	document.getElementById(hidden).style.display='none';
 	return false;
 }
-var snd_modern_ui_click = new Audio("assets/click.wav");
-var snd_modern_ui_whoosh = new Audio("assets/whoosh.ogg");
+var snd_modern_ui_navigate = new Audio("assets/navigate.wav");
 var snd_modern_ui_notify = new Audio("assets/notify.wav");
