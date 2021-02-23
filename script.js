17ea46b103ac6ca5377b1fd5b11db29852a81cfd
indianajmh <76800857+indianajmh@users.noreply.github.com>
2021-02-23 15:38:59 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ function show(shown, hidden) {
 }
 var snd_modern_ui_navigate = new Audio("assets/navigate.wav");
 var snd_modern_ui_notify = new Audio("assets/notify.wav");
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("Dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" Active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " Active";
+}
